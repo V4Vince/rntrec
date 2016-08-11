6f0583318d13d7efa816578d7f610d6fe973f46a
V4Vince <vince.guoo@gmail.com>
2016-08-11 19:23:01 -0400

    Unserializes house in Unit serializer

--- app/serializers/unit_serializer.rb
+++ app/serializers/unit_serializer.rb
@@ -1,3 +1,6 @@
 class UnitSerializer < ActiveModel::Serializer
-  attributes :id, :unit_num, :num_bedrooms, :num_bathrooms, :num_parking, :house, :expenses, :contract
+  attributes :id, :unit_num, :num_bedrooms, :num_bathrooms, :num_parking, :expenses, :contract
+
+  #serializing house will cause ember to throw an error after creating a unit.
+  # :house
 end
