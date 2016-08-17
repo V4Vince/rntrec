e7838527c556754754d2b7643090dd135fc22b41
V4Vince <vince.guoo@gmail.com>
2016-08-17 18:10:49 -0400

    Adds has_many to house serializer

--- app/serializers/house_serializer.rb
+++ app/serializers/house_serializer.rb
@@ -1,3 +1,6 @@
 class HouseSerializer < ActiveModel::Serializer
-  attributes :id, :street_name, :num_units, :purchase_price, :current_price, :mortgage, :taxes, :insurance, :user, :units, :contracts, :expenses
+  attributes :id, :street_name, :num_units, :purchase_price, :current_price, :mortgage, :taxes, :insurance, :user_id
+
+  has_many :units
+  has_many :expenses
 end
