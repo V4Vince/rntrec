3cd768beddae2e563793cc1a3493969284febda6
V4Vince <vince.guoo@gmail.com>
2016-09-11 19:27:01 -0400

    Removes belongs_to association
    Removes belongs_to association from contract and expense serializers

--- app/serializers/contract_serializer.rb
+++ app/serializers/contract_serializer.rb
@@ -1,4 +1,5 @@
 class ContractSerializer < ActiveModel::Serializer
   attributes :id, :start, :end, :num_tenants, :rent, :security
 
+  # belongs_to :unit
 end
--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,4 +1,6 @@
 class ExpenseSerializer < ActiveModel::Serializer
   attributes :id, :expense_for, :expense_description, :expense_amount, :expense_date
 
+  # belongs_to :unit
+  # belongs_to :house
 end
